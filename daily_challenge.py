d85f44cb2e0e864ea07ae1e73f97056b83c289ed
Alex Bang <alexbang@littlemac.lan>
2021-12-07 08:06:50 -0500

    Get leay year

--- daily_challenge.py
+++ daily_challenge.py
@@ -13,4 +13,23 @@
 # split = (round(((bill + tip)/people),2))
 # print(f"Each person should pay {split}")
 
-# Day 03
+# Day 03 Determine leap year
+'''On every year that is evenly divisible by 4
+    except every year that is evely divisible by 100
+        unless the year is also evenly divisible by 400'''
+
+year = int(input("Enter the year you want to check: "))
+# print(year)
+# leap years: 2020, 2024, 2028
+
+if year % 4 == 0:
+    #print("It's a leap year.")
+    if year % 100 == 0 and year % 400 == 0:
+        print("It's a leap year.")
+    elif year % 100 == 0:
+            print("It's not a leap year.")
+    else:
+        print("It's a lear year.")
+else:
+    print("Not a leap year.")
+
