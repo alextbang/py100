cde92fdccc41f1d65f52a4d3419781dc4d1cd7a0
Alex Bang <alexbang@littlemac.lan>
2021-12-07 08:13:44 -0500

    simpler statement

--- daily_challenge.py
+++ daily_challenge.py
@@ -24,9 +24,10 @@ year = int(input("Enter the year you want to check: "))
 
 if year % 4 == 0:
     #print("It's a leap year.")
-    if year % 100 == 0 and year % 400 == 0:
-        print("It's a leap year.")
-    elif year % 100 == 0:
+    if year % 100 == 0: 
+        if year % 400 == 0:
+            print("It's a leap year.")
+        else:
             print("It's not a leap year.")
     else:
         print("It's a lear year.")
