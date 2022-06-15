6692d0ff5254c8b39c4786b91b7c80867c1457ee
Alex Bang <alexbang@littlemac.lan>
2022-06-15 09:26:51 -0400

    calculates score correctly

--- day3.py
+++ day3.py
@@ -81,7 +81,7 @@
 
 ####################################################################### 
 
-# Love calculator
+## Love calculator
 
 
 """
@@ -124,7 +124,59 @@ Print: "Your score is 53."
 # Don't change the code below
 print("Welcome to the Love Calculator!")
 name1 = input("What is your name? \n")
+name1 = name1.lower()
 name2 = input("What is their name? \n")
+name2 = name2.lower()
 # Don't change the code above
 
-#Write your code below this line
+# Get total count for TRUE from two names
+countTRUE1 = 0
+letterT = name1.count('t')
+letterR = name1.count('r')
+letterU = name1.count('u')
+letterE = name1.count('e')
+countTRUE1 = countTRUE1+ letterT + letterR + letterU + letterE
+
+countTRUE2 = 0
+letterT = name2.count('t')
+letterR = name2.count('r')
+letterU = name2.count('u')
+letterE = name2.count('e')
+countTRUE2 = countTRUE2 + letterT + letterR + letterU + letterE
+
+totalTRUE = countTRUE1 + countTRUE2
+print("TRUE:", totalTRUE)
+
+# Get total count for LOVE from two names
+countLOVE1 = 0
+letterL = name1.count('l')
+letterO = name1.count('o')
+letterV = name1.count('v')
+letterE = name1.count('e')
+countLOVE1 = countLOVE1+ letterL + letterO + letterV + letterE
+
+countLOVE2 = 0
+letterL = name2.count('l')
+letterO = name2.count('o')
+letterV = name2.count('v')
+letterE = name2.count('e')
+countLOVE2 = countLOVE2+ letterL + letterO + letterV + letterE
+
+totalLOVE = countLOVE1 + countLOVE2
+print("LOVE:", totalLOVE)
+
+# Convert scores to string
+scoreTRUE = str(totalTRUE)
+scoreLOVE = str(totalLOVE)
+score = scoreTRUE + scoreLOVE
+print("score:", score)
+
+# interpret results
+result = int(score)
+# print(type(result))
+if result < 10 or result > 90:
+    print("Like coke and mentos!")
+elif result > 40 and result < 50:
+    print("You guys are alright.")
+else:
+    print(f"Your score is {result}.")
